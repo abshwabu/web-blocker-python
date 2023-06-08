ca9e53b852b16d061ae90da0b1cfc2beec4bc543
ashwabu <abdulazizredi90@gmail.com>
2023-06-08 23:16:59 +0300

    while loop

--- web_blocker.py
+++ web_blocker.py
@@ -1,2 +1,11 @@
+import time
+from datetime import datetime as dt
 host = 'C:\Windows\System32\drivers\etc'
 redirect = '127.0.0.1'
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,8)<dt.now()<dt(dt.now().year,dt.now().month,dt.now().day,16):
+        print('work')
+    else:
+        print('relax')
+    time.sleep(6)
