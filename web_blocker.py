7c7e639543959ca906e4e49aced00b7c33584f07
ashwabu <abdulazizredi90@gmail.com>
2023-06-09 18:13:49 +0300

    websites added

--- web_blocker.py
+++ web_blocker.py
@@ -1,8 +1,9 @@
 import time
 from datetime import datetime as dt
+host_temp='host'
 host = 'C:\Windows\System32\drivers\etc'
 redirect = '127.0.0.1'
-
+weblist = ['www.facebook.com','facebook.com']
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,8)<dt.now()<dt(dt.now().year,dt.now().month,dt.now().day,16):
         print('work')
