8b3a0e889a9fd11b021765f3bae7f633be7b516b
ashwabu <abdulazizredi90@gmail.com>
2023-06-09 18:52:23 +0300

    test work done

--- web_blocker.py
+++ web_blocker.py
@@ -5,7 +5,7 @@ host = 'C:\Windows\System32\drivers\etc'
 redirect = '127.0.0.1'
 weblist = ['www.facebook.com','facebook.com']
 while True:
-    if dt(dt.now().year,dt.now().month,dt.now().day,8)<dt.now()<dt(dt.now().year,dt.now().month,dt.now().day,23):
+    if dt(dt.now().year,dt.now().month,dt.now().day,8)<dt.now()<dt(dt.now().year,dt.now().month,dt.now().day,17):
         print('work')
         with open(host_temp,'r+') as file:
             content = file.read()
@@ -16,4 +16,11 @@ while True:
                     file.write('\n'+redirect+' '+website)
     else:
         print('relax')
+        with open(host_temp,'r+') as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in weblist):
+                    file.write(line)
+            file.truncate()
     time.sleep(6)
